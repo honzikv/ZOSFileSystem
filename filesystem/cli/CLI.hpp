4fb09638cc0da38af3e1422d93ddf0a4306f8ceb
honzikv <honzikv@students.zcu.cz>
2021-01-06 15:56:29 +0100

    doc pt3

--- filesystem/cli/CLI.hpp
+++ filesystem/cli/CLI.hpp
@@ -13,23 +13,42 @@
  */
 class CLI {
 
-      bool running = true;
+      bool running = true; // pokud running, neukonci se while loop
 
-      std::string filePath;
+      std::string filePath; // cesta k souboru
 
-      std::unique_ptr<FileSystem> fileSystem;
+      std::unique_ptr<FileSystem> fileSystem; // pointer na file system
 
+      /**
+       * Provede dany skript
+       * @param tokens - radky z precteneho souboru
+       */
       void executeScript(const std::vector<std::string>& tokens);
 
+      /**
+       * Provede dany prikaz
+       * @param tokens tokeny - slova rozdelene podle bilych znaku
+       */
       void execute(const std::vector<std::string>& tokens);
 
+      /**
+       * Tisk napovedy do konzole
+       */
       static void printHelp();
 
+      /**
+       * Rozdeli vstup na slova podle bilych znaku
+       * @param input vstup od uzivatele
+       * @return vektor s rozdelenymi slovy podle bilych znaku
+       */
       static std::vector<std::string> splitByWhitespace(const std::string& input);
 
     public:
       explicit CLI(std::string filePath);
 
+      /**
+       * Spusti samotne CLI
+       */
       void run();
 
 };
